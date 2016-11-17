81f1f50bee1b35d91186e5e8e628e0b910b34f8a
Deepak <dkumar431@gmail.com>
2016-11-17 09:39:26 +0530

    added user roles model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: {super_manager: 1, manager: 2, client: 3, guest: 4}         
 end
